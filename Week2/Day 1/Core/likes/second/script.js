4ad1b7426631d93aa4910f44c9720cb81590b8bf
SELLINII <msellini73@gmail.com>
2023-01-24 21:51:29 +0100

    likes

---
+++ Week2/Day 1/Core/likes/second/script.js
@@ -0,0 +1,9 @@
+function counter(lik){
+document.getElementById(lik).innerText ++;
+}
+function counter1(liks){
+ document.getElementById(liks).innerText ++;
+}
+function counter2(likt){
+    document.getElementById(likt).innerText ++;
+}
